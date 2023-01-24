24fe07622e6a7e0fd90a1d82e358ec1c13cad8f9
Alisa <amber-cloud@ya.ru>
2023-01-24 13:13:44 +0100

    24012023 main app logic

---
+++ lib/discuss/data/topic_data.ex
@@ -0,0 +1,27 @@
+defmodule Discuss.TopicData do
+  alias Discuss.Repo
+  alias Discuss.Topic
+
+  def get_topics() do
+    Repo.all(Topic)
+  end
+
+  def get_topic(topic_id) do
+    Repo.get(Topic, topic_id)
+  end
+  def get_topic!(topic_id) do
+    Repo.get!(Topic, topic_id)
+  end
+
+  def add_topic(changeset) do
+    Repo.insert(changeset)
+  end
+  
+  def update_topic(changeset) do
+    Repo.update(changeset)
+  end
+  
+  def delete_topic!(changeset) do
+    Repo.delete!(changeset)
+  end
+end
--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,10 +1,60 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
-  alias DiscussWeb.Topic
+  alias Discuss.Topic
+  alias Discuss.TopicData
+  def index(conn, _params \\ %{}) do
+    render(conn, "index.html", topics: TopicData.get_topics())
+  end
 
-  def new(conn, params) do
+  def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
-    render(conn, "new.html")
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+    case TopicData.add_topic(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: Routes.topic_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> put_flash(:error, "Please submit a valid topic")
+        |> render("new.html", changeset: changeset)
+    end
+  end
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = TopicData.get_topic(topic_id)
+    render(conn, "edit.html", changeset: Topic.changeset(topic), topic: topic)
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic}) do
+    old_topic = TopicData.get_topic(topic_id)
+    changeset = Topic.changeset(old_topic, topic)
+
+    case TopicData.update_topic(changeset) do
+      {:ok, _topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: Routes.topic_path(conn, :index))
+      {:error, changeset} ->
+        conn
+        |> put_flash(:error, "Please submit a valid topic")
+        |> render("new.html", changeset: changeset, topic: old_topic)
+    end
+  end
+  
+  def delete(conn, %{"id" => topic_id}) do
+    topic_id
+    |> TopicData.get_topic!()
+    |> Topic.changeset()
+    |> TopicData.delete_topic!()
+
+    conn
+    |> put_flash(:info, "Topic Deleted")
+    |> redirect(to: Routes.topic_path(conn, :index))
   end
 end
